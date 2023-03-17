90c15cd78192520b9b6fc3f485c2ebf78ec32008
Lucas Tymen <lucas.tymen@gmail.com>
2023-03-17 12:10:17 +0100

    step11

--- public/script.js
+++ public/script.js
@@ -1,39 +1,35 @@
-const tmdbKey = 'b7c6350175f8e20339caca474320ede3';
-const tmdbBaseUrl = 'https://api.themoviedb.org/3';
-const playBtn = document.getElementById('playBtn');
+const tmdbKey = "b7c6350175f8e20339caca474320ede3";
+const tmdbBaseUrl = "https://api.themoviedb.org/3";
+const playBtn = document.getElementById("playBtn");
 
 const getGenres = async () => {
-    const genreRequestEndpoint = '/genre/movie/list';
-    const requestParams = `?api_key=${tmdbKey}`;
-    const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+  const genreRequestEndpoint = "/genre/movie/list";
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
 
-    try{
-      const response = await fetch(urlToFetch);
-      if (response.ok){
-        const jsonResponse = await response.json();
-        return genres;
-      }
-    } catch (error){
-      console.log(error);
-    };
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return genres;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const getMovies = () => {
   const selectedGenre = getSelectedGenre();
-
 };
 
-const getMovieInfo = () => {
-
-};
+const getMovieInfo = () => {};
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
 const showRandomMovie = () => {
-  const movieInfo = document.getElementById('movieInfo');
+  const movieInfo = document.getElementById("movieInfo");
   if (movieInfo.childNodes.length > 0) {
     clearCurrentMovie();
-  };
-
+  }
 };
 
 getGenres().then(populateGenreDropdown);
